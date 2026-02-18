5378abf09d3d43fe664742929b8a7f72f12ffa02
badrman <bbadrman@gmail.com>
2026-02-18 22:58:09 +0000

    rebuild

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/QuizReactInformatique">
+    <BrowserRouter basename="/qsmReact">
       <App />
     </BrowserRouter>
   </React.StrictMode>
